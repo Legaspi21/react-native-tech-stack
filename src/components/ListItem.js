39211fde2359132fa5d8ef6493582815f0adb41f
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-02-23 16:43:22 -0800

    Wrap description in CardSection component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { 
+	Text, 
+	TouchableWithoutFeedback, 
+	View,
+	LayoutAnimation 
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 
 class ListItem extends Component {
+	componentWillUpdate() {
+		LayoutAnimation.spring();
+	}
+
 	renderDescription() {
 		const { library, expanded } = this.props;
 
 		if (expanded) {
 			return (
-				<Text>{library.description}</Text>
+				<CardSection>
+					<Text style={{ flex: 1 }}>{library.description}</Text>
+				</CardSection>
 			);
 		}
 	}
@@ -45,7 +56,7 @@ const styles = {
 };
 
 const mapStateToProps = (state, ownProps) => {
-	const expanded = state.selectedLibraryId === ownProps.library.id
+	const expanded = state.selectedLibraryId === ownProps.library.id;
 
 	return { expanded };
 };
